f3ef3e278318db4b3bba052f01ce0b76bcf30e30
matthew-kearney <matthew.kearney816@gmail.com>
2021-01-21 17:40:21 -0600

    started the work on the amazon clone. so far set up project with React and firebase. React Router Dom Routes set up and functioning. Ended with working on the Nav bar Component.

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Amazon-Clone
+//notes
+config for later
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBagXO5YVLYLqGJ8xWNvlOxqMMmRdnpxvQ",
+  authDomain: "mk--clone.firebaseapp.com",
+  projectId: "mk--clone",
+  storageBucket: "mk--clone.appspot.com",
+  messagingSenderId: "133841197004",
+  appId: "1:133841197004:web:7f3bdb1b9c391709836f69",
+  measurementId: "G-KBWWTR841E"
+};
+//
---
+++ amazon-clone/src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./Header.css"
+function Header() {
+    return (
+        <nav className="header">
+            {/*logo on left */}
+            <img
+                className="header"
+                scr="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+                alt=""
+            />
+            {/*search box */}
+            {/* links */}
+            {/* basket icon */}
+        </nav>
+    );
+}
+
+export default Header
+
+
+//started with rfce (auto created blank starting point with correct conponent name)
